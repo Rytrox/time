7f7682d9c23408cea0dd71109bb326edbc4092bd
Timo Taubmann (Timeout) <timeout@rytrox.de>
2026-07-08 18:59:21 +0200

    Replaced tsc with tsup

---
+++ tsup.config.mjs
@@ -0,0 +1,15 @@
+import {defineConfig} from 'tsup'
+
+export default defineConfig({
+    entry: ['src/index.ts', 'src/yup/index.ts'],
+    format: ['esm', 'cjs'],
+    splitting: true,
+    sourcemap: true,
+    clean: true,
+    // Hier zwingen wir den DTS-Compiler, die Warnung zu ignorieren
+    dts: {
+        compilerOptions: {
+            ignoreDeprecations: '6.0'
+        }
+    }
+})
